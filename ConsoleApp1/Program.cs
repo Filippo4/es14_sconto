f23cb5b2ab7ee95826ccadebef3896b04f00d734
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:20:56 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sconto;
+            Console.WriteLine("inserire il prezzo del primo prodotto!");
+            double primo_prodotto = double.Parse(Console.ReadLine());
+            Console.WriteLine("inserire il prezzo del secondo prodotto!");
+            double secondo_prodotto = double.Parse(Console.ReadLine());
+            if (primo_prodotto > secondo_prodotto)
+            {
+                sconto = secondo_prodotto / 2;
+                secondo_prodotto = secondo_prodotto - sconto;
+                double importoTot = primo_prodotto + secondo_prodotto;
+                Console.WriteLine($"l'importo da pagare è {importoTot}");
+            }
+            else
+
+            {
+                sconto = primo_prodotto / 2;
+                primo_prodotto = primo_prodotto - sconto;
+                double importoTot = primo_prodotto + primo_prodotto;
+                Console.WriteLine($"l'importo da pagare è {importoTot}");
+            }
+            Console.ReadLine();
+        
+        }
+    }
+}
